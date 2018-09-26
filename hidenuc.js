050037e8bdc1332abfbdfd36747d0a5f3f297574
Welz <35234925+WELZ-gh@users.noreply.github.com>
2018-09-26 03:54:18 -0400

    Update hidenuc.js

--- hidenuc.js
+++ hidenuc.js
@@ -4,14 +4,15 @@
 // @version      1
 // @description  Hides the annoying "New Contributor Indicator" so that you don't see it
 // @author       WELZ
-// @match       *://*.askubuntu.com/*
-// @match       *://*.mathoverflow.net/*
-// @match       *://*.serverfault.com/*
-// @match       *://*.stackapps.com/*
-// @match       *://*.stackexchange.com/*
-// @match       *://*.stackoverflow.com/*
-// @match       *://*.superuser.com/*
+// @match        *://*.askubuntu.com/*
+// @match        *://*.mathoverflow.net/*
+// @match        *://*.serverfault.com/*
+// @match        *://*.stackapps.com/*
+// @match        *://*.stackexchange.com/*
+// @match        *://*.stackoverflow.com/*
+// @match        *://*.superuser.com/*
 // @grant        none
+// @updateURL    https://raw.githubusercontent.com/WELZ-gh/userscripts/master/hidenuc.js
 // ==/UserScript==
 
 $(".new-contributor-indicator").css({ "display" : "none" });
