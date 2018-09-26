96f8257180d5d29d002aa3b68e10b512b26490a9
Welz <35234925+WELZ-gh@users.noreply.github.com>
2018-09-26 18:20:41 -0400

    Update hidenci.user.js

--- hidenci.user.js
+++ hidenci.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         Hide New Contributor Indicator
 // @namespace    http://tampermonkey.net/
-// @version      1
-// @description  Hides the annoying "New Contributor Indicator" so that you don't see it
+// @version      1.0
+// @description  Hides the "New Contributor Indicator" for those who'd rather not see it.
 // @author       WELZ
 // @match        *://*.askubuntu.com/*
 // @match        *://*.mathoverflow.net/*
